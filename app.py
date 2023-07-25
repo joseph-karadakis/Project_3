cda423f94326c2924d205d9e603f40c87fd3e34e
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 <joseph.karadakis@gmail.com>
2023-07-24 23:09:17 -0700

    update db2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, make_response
 import sqlite3
 import pandas as pd
 import json
@@ -11,34 +11,37 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-# Function to get the data for a specific column
-def get_column_data(conn, column_name='January/20'):
-    query = f"SELECT `Lat`, `Long`, `{column_name}` FROM confirmed;"
-    df_column = pd.read_sql_query(query, conn)
-    return df_column
+# Function to get data for a specific table and date
+def get_data(conn, table, date='2023-01-01'):
+    table_column = table.replace(' ', '_')  # Replace spaces in table name with underscores
+    query = f"SELECT `Country_Region`, `Lat`, `Long`, `{table_column}` FROM {table} WHERE date=?;"
+    df_data = pd.read_sql_query(query, conn, params=[date])
+    return df_data.to_dict(orient='records')
 
 # Home page route
 @app.route('/')
 def index():
     conn = get_db_connection()
-    columns = conn.execute("PRAGMA table_info(confirmed);").fetchall()
-    column_names = [col['name'] for col in columns if col['name'] not in ['Lat', 'Long']]
+    tables = ['confirmed', 'deaths', 'recovery']
+    dates = conn.execute("SELECT DISTINCT date FROM confirmed;").fetchall()
+    dates = [date['date'] for date in dates]
     conn.close()
 
-    return render_template('index.html', columns=column_names)
+    return render_template('index.html', tables=tables, dates=dates)
 
-# Route to get data for a specific column
-@app.route('/column/<column_name>')
-def get_column(column_name):
-    conn = get_db_connection()
-    df_column = get_column_data(conn, column_name)
-    conn.close()
+# Route to get data for a specific table and date
+@app.route('/data/<table>/<date>')
+def get_table_data(table, date):
+    try:
+        conn = get_db_connection()
+        df_data = get_data(conn, table, date)
+        conn.close()
 
-    # Convert the DataFrame to a JSON string
-    data_json = df_column.to_json(orient='records')
-    data = json.loads(data_json)
-
-    return jsonify(data)
+        # Convert the DataFrame to a JSON string
+        data_json = json.dumps(df_data)
+        return make_response(data_json, 200)
+    except Exception as e:
+        return make_response(jsonify({'error': str(e)}), 500)
 
 if __name__ == '__main__':
     app.run(debug=True)
