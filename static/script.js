52d1d1324111c0b314c4588690de8da1b3f8bd1a
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 <joseph.karadakis@gmail.com>
2023-07-24 23:09:17 -0700

    update db2

--- static/script.js
+++ static/script.js
@@ -1,31 +1,69 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Function to fetch the data for the selected column from the Flask route
-    function updateMap(selectedColumn) {
-        fetch('/column/' + selectedColumn)
-            .then(response => response.json())
+    let myMap = L.map('map').setView([0, 0], 2);
+
+    // Adding the tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(myMap);
+
+    let heat = null;
+
+    // Function to fetch the data for the selected table and date from the Flask route
+    function updateMap(selectedTable, selectedDate) {
+        fetch(`/data/${selectedTable}/${selectedDate}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
             .then(data => {
-                let myMap = L.map('map').setView([-32.8, 117.9], 7);
+                if (!Array.isArray(data)) {
+                    throw new Error('Data is not in the expected format');
+                }
 
-                // Adding the tile layer
-                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                }).addTo(myMap);
+                if (heat !== null) {
+                    myMap.removeLayer(heat);
+                }
 
-                let heatArray = data.map(row => [row.Lat, row.Long, row[selectedColumn]]);
+                console.log(data); // Debug: Check the data format received from the server
 
-                let heat = L.heatLayer(heatArray, {
+                let heatArray = data.map(row => [row.Lat, row.Long, row[selectedTable]]);
+                console.log(heatArray); // Debug: Check the heatArray format
+
+                heat = L.heatLayer(heatArray, {
                     radius: 20,
                     blur: 35,
                     maxZoom: 18,
+                    gradient: {
+                        0.4: 'green',
+                        0.6: 'yellow',
+                        1.0: 'red',
+                    },
                 }).addTo(myMap);
+            })
+            .catch(error => {
+                console.error('Error:', error.message);
             });
     }
 
-    // Call the updateMap function with the default selected column
-    updateMap(document.getElementById('column').value);
+    // Get the default selected table and date
+    const selectedTable = document.getElementById('table').value;
+    const selectedDate = document.getElementById('date').value;
 
-    // Add an event listener to the dropdown list to update the map when a new column is selected
-    document.getElementById('column').addEventListener('change', function() {
-        updateMap(this.value);
+    // Call the updateMap function with the default selected table and date
+    updateMap(selectedTable, selectedDate);
+
+    // Add event listeners to the dropdown lists to update the map when a new table or date is selected
+    document.getElementById('table').addEventListener('change', function() {
+        const selectedTable = this.value;
+        const selectedDate = document.getElementById('date').value;
+        updateMap(selectedTable, selectedDate);
+    });
+
+    document.getElementById('date').addEventListener('change', function() {
+        const selectedTable = document.getElementById('table').value;
+        const selectedDate = this.value;
+        updateMap(selectedTable, selectedDate);
     });
 });
