8aa31084ae9d0af30ca270683314f6139f111e95
emedill <124478834+emedill@users.noreply.github.com>
2023-07-29 15:35:25 -0700

    finally the values are correct. most recent code

--- compare_app.py
+++ compare_app.py
@@ -13,10 +13,12 @@ def get_db_connection():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        country = request.form['country']
+        country1 = request.form['country1']
+        country2 = request.form['country2']
         selected_table = request.form['table']
     else:
-        country = 'US'
+        country1 = 'US'
+        country2 = 'Japan'
         selected_table = 'confirmed'
 
     # Get a list of all countries from the database
@@ -28,35 +30,48 @@ def index():
     # Prepare data for visualization
     conn = get_db_connection()
     column_name = 'confirmed_cases' if selected_table == 'confirmed' else 'deaths' if selected_table == 'deaths' else 'recovery'
-    cursor = conn.execute(f"SELECT date, {column_name} as cases FROM {selected_table} WHERE Country_Region = ? ORDER BY date", (country,))
-    country_data = cursor.fetchall()
+    
+    # Fetch data for country 1
+    cursor = conn.execute(f"SELECT date, {column_name} as cases FROM {selected_table} WHERE Country_Region = ? ORDER BY date", (country1,))
+    country1_data = cursor.fetchall()
+
+    # Fetch data for country 2
+    cursor = conn.execute(f"SELECT date, {column_name} as cases FROM {selected_table} WHERE Country_Region = ? ORDER BY date", (country2,))
+    country2_data = cursor.fetchall()
+
     conn.close()
 
     data = {
-        country: [{'date': row['date'], 'cases': row['cases']} for row in country_data]
+        'Country1': [{'date': row['date'], 'cases': row['cases']} for row in country1_data],
+        'Country2': [{'date': row['date'], 'cases': row['cases']} for row in country2_data]
     }
 
-    return render_template('compare_index.html', countries=countries, selected_country1=country,
+    return render_template('compare_index.html', countries=countries, selected_country1=country1, selected_country2=country2,
                            selected_table=selected_table, data=data)
 
 @app.route('/compare', methods=['POST'])
 def compare():
-    country = request.json['country']
+    country1 = request.json['country1']
+    country2 = request.json['country2']
     table = request.json['table']
 
-    # Get data for the selected country from the selected table in the database
+    # Get data for the selected countries from the selected table in the database
     conn = get_db_connection()
     column_name = 'confirmed_cases' if table == 'confirmed' else 'deaths' if table == 'deaths' else 'recovery'
-    cursor = conn.execute(f'SELECT date, {column_name} FROM {table} WHERE Country_Region = ? ORDER BY date', (country,))
-    country_data = cursor.fetchall()
+    cursor = conn.execute(f'SELECT date, {column_name} FROM {table} WHERE Country_Region = ? ORDER BY date', (country1,))
+    country1_data = cursor.fetchall()
+    cursor = conn.execute(f'SELECT date, {column_name} FROM {table} WHERE Country_Region = ? ORDER BY date', (country2,))
+    country2_data = cursor.fetchall()
     conn.close()
 
     # Prepare data for visualization
     data = {
-        country: [{'date': row['date'], 'cases': row[column_name]} for row in country_data]
+        'Country1': [{'date': row['date'], 'cases': row[column_name]} for row in country1_data],
+        'Country2': [{'date': row['date'], 'cases': row[column_name]} for row in country2_data]
     }
 
     return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
