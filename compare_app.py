1ba2b42b2ad573fc82079cf622ad0d3bc0167ed2
emedill <124478834+emedill@users.noreply.github.com>
2023-07-29 15:16:57 -0700

    values are finally correct, only 1 country
    still working on it. fixed the issue with the values not displaying properly. had to test it with just one country to make it easier. adding in the next country drop down

--- compare_app.py
+++ compare_app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 import sqlite3
-import pandas as pd
-import json
 
 app = Flask(__name__)
 
@@ -15,12 +13,10 @@ def get_db_connection():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        country1 = request.form['country1']
-        country2 = request.form['country2']
+        country = request.form['country']
         selected_table = request.form['table']
     else:
-        country1 = 'US'
-        country2 = 'India'
+        country = 'US'
         selected_table = 'confirmed'
 
     # Get a list of all countries from the database
@@ -32,39 +28,32 @@ def index():
     # Prepare data for visualization
     conn = get_db_connection()
     column_name = 'confirmed_cases' if selected_table == 'confirmed' else 'deaths' if selected_table == 'deaths' else 'recovery'
-    cursor = conn.execute(f'SELECT date, SUM({column_name}) as total_cases FROM {selected_table} WHERE Country_Region = ? GROUP BY date ORDER BY date', (country1,))
-    country1_data = cursor.fetchall()
-    cursor = conn.execute(f'SELECT date, SUM({column_name}) as total_cases FROM {selected_table} WHERE Country_Region = ? GROUP BY date ORDER BY date', (country2,))
-    country2_data = cursor.fetchall()
+    cursor = conn.execute(f"SELECT date, {column_name} as cases FROM {selected_table} WHERE Country_Region = ? ORDER BY date", (country,))
+    country_data = cursor.fetchall()
     conn.close()
 
     data = {
-        'Country1': [{'date': row['date'], 'cases': row['total_cases']} for row in country1_data],
-        'Country2': [{'date': row['date'], 'cases': row['total_cases']} for row in country2_data]
+        country: [{'date': row['date'], 'cases': row['cases']} for row in country_data]
     }
 
-    return render_template('compare_index.html', countries=countries, tables=['confirmed', 'deaths', 'recovery'],
-                           selected_country1=country1, selected_country2=country2, selected_table=selected_table, data=data)
+    return render_template('compare_index.html', countries=countries, selected_country1=country,
+                           selected_table=selected_table, data=data)
 
 @app.route('/compare', methods=['POST'])
 def compare():
-    country1 = request.json['country1']
-    country2 = request.json['country2']
+    country = request.json['country']
     table = request.json['table']
 
-    # Get data for the selected countries from the selected table in the database
+    # Get data for the selected country from the selected table in the database
     conn = get_db_connection()
     column_name = 'confirmed_cases' if table == 'confirmed' else 'deaths' if table == 'deaths' else 'recovery'
-    cursor = conn.execute(f'SELECT date, {column_name} FROM {table} WHERE Country_Region = ? ORDER BY date', (country1,))
-    country1_data = cursor.fetchall()
-    cursor = conn.execute(f'SELECT date, {column_name} FROM {table} WHERE Country_Region = ? ORDER BY date', (country2,))
-    country2_data = cursor.fetchall()
+    cursor = conn.execute(f'SELECT date, {column_name} FROM {table} WHERE Country_Region = ? ORDER BY date', (country,))
+    country_data = cursor.fetchall()
     conn.close()
 
     # Prepare data for visualization
     data = {
-        'Country1': [{'date': row['date'], 'cases': row[column_name] // 1000} for row in country1_data],
-        'Country2': [{'date': row['date'], 'cases': row[column_name] // 1000} for row in country2_data]
+        country: [{'date': row['date'], 'cases': row[column_name]} for row in country_data]
     }
 
     return jsonify(data)
